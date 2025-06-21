492cb4cc2144cc3a305e31deee27a7abe746fd70
Hungnd <hungnd@navisoft.com.vn>
2025-06-21 13:41:46 +0700

    Remove Duplicates from Sorted Array

---
+++ LeetCode/Sln_26.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int RemoveDuplicates(int[] nums)
+        {
+            List<int> lst = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!lst.Contains(nums[i]))
+                {
+                    lst.Add(nums[i]);
+                }
+            }
+
+            for (int i = 0; i < lst.Count; i++)
+            {
+                nums[i] = lst[i];
+            }
+
+            return lst.Count;
+        }
+    }
+}
---
+++ LeetTest/Test_26.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetTest
+{
+    public class Test_26 : TestInit
+    {
+        [Fact]
+        public void Case1()
+        {
+            int[] nums = [1, 1, 2];
+            int[] expectedNums = new int[] { 1, 2 };
+
+            int k = _solution.RemoveDuplicates(nums);
+
+            Assert.Equal(expectedNums.Length, k);
+            for (int i = 0; i < k; i++)
+            {
+                Assert.Equal(expectedNums[i], nums[i]);
+            }
+        }
+
+        [Fact]
+        public void Case2()
+        {
+            int[] nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+            int[] expectedNums = new int[] { 0, 1, 2, 3, 4 };
+
+            int k = _solution.RemoveDuplicates(nums);
+
+            Assert.Equal(expectedNums.Length, k);
+            for (int i = 0; i < k; i++)
+            {
+                Assert.Equal(expectedNums[i], nums[i]);
+            }
+        }
+    }
+}
