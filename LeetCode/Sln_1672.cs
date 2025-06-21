0b685b55737a032ffa8163a351a8cf93681cda0d
Hungnd <hungnd@navisoft.com.vn>
2025-06-21 13:39:55 +0700

    Richest Customer Wealth

---
+++ LeetCode/Sln_1672.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/richest-customer-wealth/
+        /// </summary>
+        /// <param name="accounts"></param>
+        /// <returns></returns>
+        public int MaximumWealth(int[][] accounts)
+        {
+            int count = accounts.Length;
+            int result = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < accounts[i].Length; j++)
+                {
+                    sum += accounts[i][j];
+                }
+
+                if (sum > result)
+                {
+                    result = sum;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetTest/Test_1672.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetTest
+{
+    public class Test_1672 : TestInit
+    {
+        [Fact]
+        public void Case1()
+        {
+            int[][] input = [[1, 2, 3], [3, 2, 1]];
+            int expected = 6;
+            int actual = _solution.MaximumWealth(input);
+            
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Case2()
+        {
+            int[][] input = [[1, 5], [7, 3], [3, 5]];
+            int expected = 10;
+            int actual = _solution.MaximumWealth(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Case3()
+        {
+            int[][] input = [[2, 8, 7], [7, 1, 3], [1, 9, 5]];
+            int expected = 17;
+            int actual = _solution.MaximumWealth(input);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
