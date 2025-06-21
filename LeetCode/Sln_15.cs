321dc146e229152e6faf51ff48cbd1b5807d2c62
Hungnd <hungnd@navisoft.com.vn>
2025-06-21 13:42:09 +0700

    3Sum

---
+++ LeetCode/Sln_15.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/3sum/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Array.Sort(nums);
+            IList<IList<int>> res = new List<IList<int>>();
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+                int left = i + 1, right = nums.Length - 1;
+                while (left < right)
+                {
+                    int sum = nums[i] + nums[left] + nums[right];
+                    if (sum == 0)
+                    {
+                        res.Add(new List<int>() { nums[i], nums[left], nums[right] });
+                        while (left < right && nums[right - 1] == nums[right]) right--;
+                        while (left < right && nums[left + 1] == nums[left]) left++;
+                        left++; right--;
+                    }
+                    else if (sum < 0) left++;
+                    else right--;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ LeetTest/Test_15.cs
@@ -0,0 +1,90 @@
+﻿using LeetCode;
+
+namespace LeetTest
+{
+    public class Test_15 : TestInit
+    {
+        // Custom comparer để hỗ trợ Assert.Equal với danh sách lồng nhau
+        public class SequenceEqualComparer<T> : IEqualityComparer<T> where T : IEnumerable<int>
+        {
+            public bool Equals(T x, T y) => x.SequenceEqual(y);
+            public int GetHashCode(T obj)
+            {
+                unchecked
+                {
+                    return obj.Aggregate(17, (hash, val) => hash * 31 + val.GetHashCode());
+                }
+            }
+        }
+
+        [Fact]
+        public void Case1()
+        {
+            int[] nums = new[] { -1, 0, 1, 2, -1, -4 };
+
+            var expected = new List<IList<int>>
+            {
+                new List<int> { -1, -1, 2 },
+                new List<int> { -1, 0, 1 }
+            };
+
+            var actual = _solution.ThreeSum(nums);
+
+            // So sánh không cần đúng thứ tự bộ ba, nhưng cần nội dung giống nhau
+            Assert.Equal(
+                expected.Select(triplet => triplet.OrderBy(x => x)).OrderBy(t => string.Join(",", t)),
+                actual.Select(triplet => triplet.OrderBy(x => x)).OrderBy(t => string.Join(",", t)),
+                new SequenceEqualComparer<IEnumerable<int>>()
+            );
+        }
+
+        [Fact]
+        public void Case2_NoTripletSumsToZero()
+        {
+            int[] nums = new[] { 0, 1, 1 };
+
+            var expected = new List<IList<int>>(); // Empty result
+            var actual = _solution.ThreeSum(nums);
+
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void Case3_AllZerosTriplet()
+        {
+            int[] nums = new[] { 0, 0, 0 };
+
+            var expected = new List<IList<int>>
+            {
+                new List<int> { 0, 0, 0 }
+            };
+
+            var actual = _solution.ThreeSum(nums);
+
+            Assert.Equal(
+                expected.Select(t => t.OrderBy(x => x)).OrderBy(t => string.Join(",", t)),
+                actual.Select(t => t.OrderBy(x => x)).OrderBy(t => string.Join(",", t)),
+                new SequenceEqualComparer<IEnumerable<int>>()
+            );
+        }
+
+        [Fact]
+        public void Case4()
+        {
+            int[] nums = new[] { 1, -1, -1, 0 };
+
+            var expected = new List<IList<int>>
+            {
+                new List<int> { -1, 0, 1 }
+            };
+
+            var actual = _solution.ThreeSum(nums);
+
+            Assert.Equal(
+                expected.Select(t => t.OrderBy(x => x)).OrderBy(t => string.Join(",", t)),
+                actual.Select(t => t.OrderBy(x => x)).OrderBy(t => string.Join(",", t)),
+                new SequenceEqualComparer<IEnumerable<int>>()
+            );
+        }
+    }
+}
