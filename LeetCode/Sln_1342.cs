fd916b3cc079e14948fa57619729c25f556616fb
Hungnd <hungnd@navisoft.com.vn>
2025-06-21 13:40:39 +0700

    Number of Steps to Reduce a Number to Zero

---
+++ LeetCode/Sln_1342.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public int NumberOfSteps(int num)
+        {
+            int n = 0;
+            while (num != 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                    n++;
+                }
+                else
+                {
+                    num -= 1;
+                    n++;
+                }
+            }
+            return n;
+        }
+    }
+}
---
+++ LeetTest/Test_1342.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetTest
+{
+    public class Test_1342 : TestInit
+    {
+        [Fact]
+        public void Case1()
+        {
+            int input = 14;
+            int expected = 6;
+            int actual = _solution.NumberOfSteps(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Case2()
+        {
+            int input = 8;
+            int expected = 4;
+            int actual = _solution.NumberOfSteps(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Case3()
+        {
+            int input = 123;
+            int expected = 12;
+            int actual = _solution.NumberOfSteps(input);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
