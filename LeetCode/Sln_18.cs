f90eeccf735f5cb0be15caacc1000c7084b7338c
Hungnd <hungnd@navisoft.com.vn>
2025-06-21 13:42:50 +0700

    4Sum

---
+++ LeetCode/Sln_18.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/4sum/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int sum = 0;
+            List<IList<int>> res = new List<IList<int>>();
+            for (int i = 0; i < nums.Length - 3; i++)
+            {
+                for (int j = 0; j < nums.Length - 4; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        continue;
+                    }
+                    int left = j + 1, right = nums.Length - 1;
+                    while (left < right)
+                    {
+                        sum = nums[i] + nums[j] + nums[left] + nums[right];
+                        if (sum == target)
+                        {
+                            res.Add(new List<int>() { nums[i], nums[j], nums[left], nums[right] });
+                            while (left < right && nums[right - 1] == nums[right]) right--;
+                            while (left < right && nums[left + 1] == nums[left]) left++;
+                            right--; left++;
+                        }
+                        else if (sum < target) left++;
+                        else right--;                        
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ LeetTest/Test_18.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetTest.Test_15;
+
+namespace LeetTest
+{
+    public class Test_18 : TestInit
+    {
+        [Fact]
+        public void Case1()
+        {
+            int[] nums = new[] { 1, 0, -1, 0, -2, 2 };
+            int target = 0;
+
+            var expected = new List<IList<int>>
+            {
+                new List<int> { -2,-1,1,2 },
+                new List<int> { -2, 0, 0, 2 },
+                new List<int> { -1, 0, 0, 1 }
+            };
+
+            var actual = _solution.FourSum(nums, target);
+
+            // So sánh không cần đúng thứ tự bộ ba, nhưng cần nội dung giống nhau
+            Assert.Equal(
+                expected.Select(triplet => triplet.OrderBy(x => x)).OrderBy(t => string.Join(",", t)),
+                actual.Select(triplet => triplet.OrderBy(x => x)).OrderBy(t => string.Join(",", t)),
+                new SequenceEqualComparer<IEnumerable<int>>()
+            );
+        }
+
+        [Fact]
+        public void Case2()
+        {
+            int[] nums = new[] { 2, 2, 2, 2, 2 };
+            int target = 8;
+
+            var expected = new List<IList<int>>
+            {
+                new List<int> { 2, 2, 2, 2}
+            };
+
+            var actual = _solution.FourSum(nums, target);
+
+            // So sánh không cần đúng thứ tự bộ ba, nhưng cần nội dung giống nhau
+            Assert.Equal(
+                expected.Select(triplet => triplet.OrderBy(x => x)).OrderBy(t => string.Join(",", t)),
+                actual.Select(triplet => triplet.OrderBy(x => x)).OrderBy(t => string.Join(",", t)),
+                new SequenceEqualComparer<IEnumerable<int>>()
+            );
+        }
+    }
+}
