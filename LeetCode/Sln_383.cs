dd455aeff35c84b53892144c95d563466b8b7890
Hungnd <hungnd@navisoft.com.vn>
2025-06-21 13:41:23 +0700

    Ransom Note

---
+++ LeetCode/Sln_383.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/ransom-note/
+        /// </summary>
+        /// <param name="ransomNote"></param>
+        /// <param name="magazine"></param>
+        /// <returns></returns>
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            Dictionary<string, int> magazineLetters = new Dictionary<string, int>();
+            int i = 0;
+            char currentLetter = '\0';
+
+            for (i = 0; i < magazine.Length; i++)
+            {
+                currentLetter = magazine.ToCharArray()[i];
+                if (magazineLetters.ContainsKey(currentLetter.ToString()))
+                {
+                    magazineLetters[currentLetter.ToString()]++;
+                }
+                else
+                {
+                    magazineLetters.Add(currentLetter.ToString(), 1);
+                }
+            }
+
+            for (i = 0; i < ransomNote.Length; i++)
+            {
+                currentLetter = ransomNote.ToCharArray()[i];
+                if (magazineLetters.ContainsKey(currentLetter.ToString()))
+                {
+                    if (magazineLetters[currentLetter.ToString()] == 0)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        magazineLetters[currentLetter.ToString()]--;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetTest/Test_383.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetTest
+{
+    public class Test_383 : TestInit
+    {
+        [Fact]
+        public void Case1()
+        {
+            string ransomNote = "a";
+            string magazine = "b";
+            bool actual = _solution.CanConstruct(ransomNote, magazine);
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void Case2()
+        {
+            string ransomNote = "aa";
+            string magazine = "ab";
+            bool actual = _solution.CanConstruct(ransomNote, magazine);
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void Case3()
+        {
+            string ransomNote = "aa";
+            string magazine = "aab";
+            bool actual = _solution.CanConstruct(ransomNote, magazine);
+
+            Assert.True(actual);
+        }
+    }
+}
