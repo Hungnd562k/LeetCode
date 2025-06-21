0726a5a26e71f8b8e8c515313df7e70200a2f6b2
Hungnd <hungnd@navisoft.com.vn>
2025-06-21 13:41:00 +0700

    izz Buzz

---
+++ LeetCode/Sln_412.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/fizz-buzz/
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public IList<string> FizzBuzz(int n)
+        {
+            bool isFizz = false; //  divisible by 3
+            bool isBuzz = false; // divisible by 5
+
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = i + 1;
+                if (num % 3 == 0)
+                {
+                    isFizz = true;
+                }
+                if (num % 5 == 0)
+                {
+                    isBuzz = true;
+                }
+
+                if (isFizz && isBuzz)
+                {
+                    result.Add("FizzBuzz");
+                }
+                else if (isFizz)
+                {
+                    result.Add("Fizz");
+                }
+                else if (isBuzz)
+                {
+                    result.Add("Buzz");
+                }
+                else
+                {
+                    result.Add(num.ToString());
+                }
+
+                isFizz = false;
+                isBuzz = false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetTest/Test_412.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetTest
+{
+    public class Test_412 : TestInit
+    {
+        [Fact]
+        public void Case1()
+        {
+            int input = 3;
+            IList<string> expected = ["1", "2", "Fizz"];
+            IList<string> actual = _solution.FizzBuzz(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Case2()
+        {
+            int input = 5;
+            IList<string> expected = ["1", "2", "Fizz", "4", "Buzz"];
+            IList<string> actual = _solution.FizzBuzz(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Case3()
+        {
+            int input = 15;
+            IList<string> expected = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"];
+            IList<string> actual = _solution.FizzBuzz(input);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
