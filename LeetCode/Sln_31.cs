7be9796ef0a5b2e5f3d06c3dbf7c3568c6ed098f
Hungnd <hungnd@navisoft.com.vn>
2025-06-22 12:43:16 +0700

    Next permutaion

---
+++ LeetCode/Sln_31.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/next-permutation/
+    /// </summary>
+    public partial class Solution
+    {
+        public void NextPermutation(int[] nums)
+        {
+            int i = nums.Length - 2, startPoint = 0;
+            int flag = -1; // Nếu duyệt hết mảng mà vẫn -1 thì nghĩa là mảng giảm dần, đảo ngược lại là đủ
+            while (i >= 0)
+            {
+                if (nums[i] < nums[i + 1])
+                {
+                    startPoint = i;
+                    flag = 0;
+
+                    // Tìm phần tử nhỏ nhất trong đoạn [nums[i + 1], .., end]
+                    for (int j = nums.Length - 1; j > i; j--)
+                    {
+                        if (nums[j] > nums[i])
+                        {
+                            Swap(nums, i, j);
+                            Reverse(nums, i + 1);
+                            return;
+                        }
+                    }
+                }
+                i--;
+            }
+
+            if (flag == -1)
+            {
+                Reverse(nums, 0);
+                return;
+            }
+        }
+
+        public void Reverse(int[] nums, int startPoint)
+        {
+            int i = startPoint, j = nums.Length - 1;
+            while (i < j)
+            {
+                if (nums[i] > nums[j]) Swap(nums, i, j);
+                i++; j--;
+            }
+        }
+
+        public void Swap(int[] nums, int i, int j)
+        {
+            int temp = 0;
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
---
+++ LeetTest/Test_31.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetTest
+{
+    public class Test_31 : TestInit
+    {
+        [Fact]
+        public void Case1()
+        {
+            int[] nums = new int[] { 1, 2, 3 };
+            int[] expected = new int[] { 1, 3, 2 };
+
+            _solution.NextPermutation(nums);
+            Assert.Equal(expected, nums);
+        }
+
+        [Fact]
+        public void Case2()
+        {
+            int[] nums = new int[] { 3, 2, 1 };
+            int[] expected = new int[] { 1, 2, 3 };
+
+            _solution.NextPermutation(nums);
+            Assert.Equal(expected, nums);
+        }
+
+        [Fact]
+        public void Case3()
+        {
+            int[] nums = new int[] { 1, 1, 5 };
+            int[] expected = new int[] { 1, 5, 1 };
+
+            _solution.NextPermutation(nums);
+            Assert.Equal(expected, nums);
+        }
+
+        [Fact]
+        public void Case4()
+        {
+            int[] nums = new int[] { 1, 3, 5, 4, 2 };
+            int[] expected = new int[] { 1, 4, 2, 3, 5 };
+
+            _solution.NextPermutation(nums);
+            Assert.Equal(expected, nums);
+        }
+    }
+}
